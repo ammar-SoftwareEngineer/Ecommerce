089917443c62f573cdf2e533e808a3a1bee06e89
ammar yasser <aboamer0548@gmail.com>
2024-02-25 13:00:29 +0200

    update

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,13 +8,26 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class RegisterComponent {
   registerForm: FormGroup = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
-    rePassword: new FormControl(''),
-    phone: new FormControl(''),
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+    ]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^\[A-Z][a-z0-9]{6,10}$/),
+    ]),
+    rePassword: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^\[A-Z][a-z0-9]{6,10}$/),
+    ]),
+    phone: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^01[0125][0-9]{8}$/),
+    ]),
   });
   handleForm(): void {
-    console.log('ammar');
+    console.log(this.registerForm.value);
   }
 }
