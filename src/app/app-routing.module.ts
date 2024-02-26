963b7b02c007ffab09afa066c0ff4b4b84f72787
ammar yasser <aboamer0548@gmail.com>
2024-02-26 09:54:03 +0200

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -28,6 +28,7 @@ const routes: Routes = [
     path: '',
     component: AuthLayoutComponent,
     children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
       { path: 'login', component: LoginComponent },
       { path: 'register', component: RegisterComponent },
     ],
