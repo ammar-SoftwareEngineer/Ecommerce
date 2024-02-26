0abb87174420b60d183cb301bae89741c529d08d
ammar yasser <aboamer0548@gmail.com>
2024-02-26 13:17:32 +0200

    update

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -24,26 +24,6 @@ export class RegisterComponent {
   msgError: string = '';
   isLoading: boolean = false;
 
-  // registerForm: FormGroup = new FormGroup({
-  //   name: new FormControl('', [
-  //     Validators.required,
-  //     Validators.minLength(3),
-  //     Validators.maxLength(20),
-  //   ]),
-  //   email: new FormControl('', [Validators.required, Validators.email]),
-  //   password: new FormControl('', [
-  //     Validators.required,
-  //     Validators.pattern(/^[A-Z][a-z0-9]{6,10}$/),
-  //   ]),
-  //   rePassword: new FormControl('', [
-  //     Validators.required,
-  //     Validators.pattern(/^[A-Z][a-z0-9]{6,10}$/),
-  //   ]),
-  //   phone: new FormControl('', [
-  //     Validators.required,
-  //     Validators.pattern(/^01[0125][0-9]{8}$/),
-  //   ]),
-  // });
   registerForm: FormGroup = this._FormBuilder.group({
     name: [
       '',
